2c18721c3fead53cb5c7e5295cfd7099b59bdd3f
bgillcode <b.gill.messages@gmail.com>
2021-04-04 23:51:21 +0100

    Initial imports

---
+++ main.py
@@ -0,0 +1,6 @@
+import time
+import sys
+import pychrome
+from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
