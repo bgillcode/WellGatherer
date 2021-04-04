cd76c83801abf80ac8c2bbe29f0d539a4c0f47f6
bgillcode <b.gill.messages@gmail.com>
2021-04-04 23:53:12 +0100

    Add checks for kwargs

--- main.py
+++ main.py
@@ -4,3 +4,22 @@ import pychrome
 from selenium import webdriver
 from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
+
+
+
+def output_on_start(**kwargs):
+    gottenOutput = kwargs
+
+
+    if filesize == 0:
+        checkingFileSizeBoolean = False
+    else:
+        checkingFileSizeBoolean = True
+
+
+
+
+
+
+def output_on_end(**kwargs):
+    gottenOutput2 = kwargs
