ec89bf406b173ac5fe523244abea2b007f6a8a2f
bgillcode <b.gill.messages@gmail.com>
2021-04-04 23:53:55 +0100

    Search for the correct input string and save to file

--- main.py
+++ main.py
@@ -10,13 +10,26 @@ from selenium.webdriver.common.desired_capabilities import DesiredCapabilities
 def output_on_start(**kwargs):
     gottenOutput = kwargs
 
+    filesize = os.path.getsize("a.txt")
 
     if filesize == 0:
         checkingFileSizeBoolean = False
     else:
         checkingFileSizeBoolean = True
 
+    with open('a' + '.txt', 'a') as f:
+        s = str(gottenOutput)
+        if s.find('search?device_type=desktop&id=' + numberFromURL) != -1:
+            print('URL for video found, storing...')
 
+            try:
+                if (checkingFileSizeBoolean == False):
+                    newGottenURL = gottenOutput['request']['url']
+                    f.write(str(newGottenURL.strip()))
+                    checkingFileSizeBoolean = True
+                    return
+            except:
+                pass
 
 
 
